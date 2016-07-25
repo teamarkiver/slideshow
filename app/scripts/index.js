c9e3194f16e7410c8d29baf2827677df60748759
rhc912 <rhc912@gmail.com>
2016-07-25 11:51:08 -0400

    tweaks

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -8,7 +8,8 @@ var router = require('./router.js');
 $.ajaxSetup({
   headers : {
     "Accept": "application/json,version=2",
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    "Authorization": "Token token=43a14bfc3896d21323cd9b8c9a7f8019"
   }
 });
 
